1cf4bb1ce13a6ae0becddbdd6a8bb74dab09cb73
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-21 16:47:14 -0300

    adding a fucntional button filling a form to include new items on a list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-
+import { useState } from 'react';
 import './App.css';
 
+let initialState = [
+  {
+    id: 1, 
+    descricao: 'Primeira Atividade',
+  },
+  {
+    id: 2,
+    descricao: 'Segunda Atividadade',
+  },
+];
+
 function App() {
-  const atividades = [
-    {
-      id: 1, 
-      descricao: 'Primeira Atividade',
-    },
-    {
-      id: 2,
-      descricao: 'Segunda Atividadade',
-    },
-  ];
+  const [atividades, setAtividades] = useState(initialState)
+
+  function addAtividade(e) {
+    e.preventDefault();
+
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
+    };
+   
+    setAtividades([ ...atividades, { ...atividade }]);   
+  }
 
   return (
     <>
     <form>
       <input id='id' type='text' placeholder='id' />
       <input id='descricao' type='text' placeholder='descricao'/>
-      <button>+ Atividade</button>
+      <button onClick={addAtividade}>+ Atividade</button>
     </form>
     <div className="mt-3">
       <ul className='list-group'>
-        {atividades.map(ativ => (
-          <li key={ativ.id} className='list-group-item'>{ativ.id} - {ativ.descricao}</li> 
-        ))}                    
+        {atividades.map((ativ) => (
+          <li key={ativ.id} className='list-group-item'>
+            {ativ.id} - {ativ.descricao}
+          </li>
+        ))}                 
       </ul>
     </div>
     </>
