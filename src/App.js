afb59d8ad3b496aa8842997ff6231e6203f100ef
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-22 15:55:07 -0300

    adding a few more properties to the card such as priority level and icons to describe them

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 let initialState = [
   {
     id: 1, 
+    prioridade: '1',
+    titulo: 'Título',
     descricao: 'Primeira Atividade',
   },
   {
     id: 2,
+    prioridade: '1',
+    titulo: 'Título',
     descricao: 'Segunda Atividadade',
   },
 ];
@@ -20,21 +24,62 @@ function App() {
 
     const atividade = {
       id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value,
     };
    
     setAtividades([ ...atividades, { ...atividade }]);   
   }
 
+  function prioridadeLabel(param){
+    switch(param) {
+      case '1':
+        return 'Baixa';
+      case '2':
+        return 'Normal';
+      case '3':
+        return 'Alta';
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function prioridadeStyle(param){
+    switch(param) {
+      case '1':
+        return 'smile';
+      case '2':
+        return 'meh';
+      case '3':
+        return 'frown';
+      default:
+        return 'Não definido';
+    }
+  }
+
   return (
     <>
     <form className='row g-3'>
       <div className="col-md-6">
-        <label for="inputEmail4" className='form-label'>Id</label>
+        <label className='form-label'>Id</label>
         <input id='id' type="text" className="form-control" />
-      </div>    
+      </div>   
       <div className="col-md-6">
-        <label for="inputEmail4" className='form-label'>Descrição</label>
+        <label className='form-label'>Prioridade</label>
+          <select id="prioridade" className="form-select">
+            <option defaultValue='0'>Selecionar...</option>
+            <option value='1'>Baixa</option>
+            <option value='2'>Normal</option>
+            <option value='3'>Alta</option>
+          </select>
+      </div> 
+      <div className="col-md-6">
+        <label className='form-label'>Título</label>
+        <input id='titulo' type="text" className="form-control" />
+      </div> 
+      <div className="col-md-6">
+        <label className='form-label'>Descrição</label>
         <input id='descricao' type="text" className="form-control" />
       </div>  
       <hr/>  
@@ -49,19 +94,27 @@ function App() {
               <div className='d-flex justify-content-between'>
                 <h5 className='card-title'>
                   <span className='badge bg-secondary me-1'>{ativ.id}</span>
-                   - título
+                   - {ativ.titulo}
                 </h5>
                 <h6>
                   Prioridade:
                   <span className='ms-1 text-black'>
-                    <i className='me-1 far fa-frown'></i>
-                    Normal
+                    <i className={'me-1 far fa-' + prioridadeStyle(ativ.prioridade)}></i>
+                    {prioridadeLabel(ativ.prioridade)}
                   </span>                  
                 </h6>
               </div>
-              <p className="card-text">
-                {ativ.descricao}                
-              </p>             
+              <p className="card-text">{ativ.descricao}</p>
+              <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+                <button className='btn btn-sm btn-outline-primary me-2'>
+                  <i className='fas fa-pen me-2'></i>
+                  Editar
+                </button>
+                <button className='btn btn-sm btn-outline-danger'>
+                <i className='fas fa-trash me-2'></i>
+                  Deletar
+                </button>
+              </div>          
             </div>
           </div>
         ))}                 
