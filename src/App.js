a8ca6336ddb86ef12f32c5ef4e73f7f18b520c79
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-22 17:01:26 -0300

    including a color function according to the priority of the input, as well as the id as a read only field and auto sum

--- src/App.js
+++ src/App.js
@@ -45,14 +45,14 @@ function App() {
     }
   }
 
-  function prioridadeStyle(param){
+  function prioridadeStyle(param, icon){
     switch(param) {
       case '1':
-        return 'smile';
+        return icon ? 'smile' : 'sucess';
       case '2':
-        return 'meh';
+        return icon ? 'meh' : 'dark';
       case '3':
-        return 'frown';
+        return icon ? 'frown' : 'warning';
       default:
         return 'Não definido';
     }
@@ -63,7 +63,7 @@ function App() {
     <form className='row g-3'>
       <div className="col-md-6">
         <label className='form-label'>Id</label>
-        <input id='id' type="text" className="form-control" />
+        <input id='id' type="text" className="form-control" readOnly value={Math.max.apply(Math, atividades.map(item => item.id)) + 1} />
       </div>   
       <div className="col-md-6">
         <label className='form-label'>Prioridade</label>
@@ -89,7 +89,7 @@ function App() {
     </form>
     <div className="mt-3">      
         {atividades.map((ativ) => (         
-          <div key={ativ.id} className="card mb-2 shadow-sm">            
+          <div key={ativ.id} className={"card mb-2 shadow-sm border-" + prioridadeStyle(ativ.prioridade)}>            
             <div className="card-body">
               <div className='d-flex justify-content-between'>
                 <h5 className='card-title'>
@@ -98,8 +98,8 @@ function App() {
                 </h5>
                 <h6>
                   Prioridade:
-                  <span className='ms-1 text-black'>
-                    <i className={'me-1 far fa-' + prioridadeStyle(ativ.prioridade)}></i>
+                  <span className={'ms-1 text-' + prioridadeStyle(ativ.prioridade)}>
+                    <i className={'me-1 far fa-' + prioridadeStyle(ativ.prioridade, true)}></i>
                     {prioridadeLabel(ativ.prioridade)}
                   </span>                  
                 </h6>
