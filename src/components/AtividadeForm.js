0b92c764d3d6491c688f781bf8741be3fc705a1b
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-23 10:32:53 -0300

    making adjustments on how the page is set up for edition on its items and including these properties

--- src/components/AtividadeForm.js
+++ src/components/AtividadeForm.js
@@ -1,32 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const atividadeInicial = {
+    id: 0,
+    titulo: '',
+    prioridade: 0,
+    descricao: ''
+}
 
 export default function AtividadeForm(props) {
-    const [atividade, setAtividade] = useState({})
+    const [atividade, setAtividade] = useState(atividadeAtual());
+
+    useEffect(() => {
+        if(props.ativSelecionada.id !== 0)
+        setAtividade(props.ativSelecionada);
+    }, [props.ativSelecionada]);
 
     const inputTextHandler = (e) => {
         const {name, value} = e.target;
 
         setAtividade({...atividade, [name]: value})
+    };
+
+    const handleCancelar = (e) => {
+        e.preventDefault();
+
+        // cancelarAtividade()
+
+        setAtividade(atividadeInicial)
     }
 
-    // Math.max.apply(
-    //     Math, 
-    //     props.atividades.map((item) => item.id)
-    //     ) + 1  
+    function atividadeAtual() {
+        if (props.ativSelecionada.id !== 0) {
+            return props.ativSelecionada;
+        }
+        else {
+            return atividadeInicial;
+        }
+    }
 
     return (
-        <form className='row g-3'>
-            <div className="col-md-6">
-                <label className='form-label'>Id</label>
-                <input 
-                    name='id'
-                    id='id' 
+        <>
+        <h1>Atividadade {atividade.id !== 0 ? atividade.id : ''}</h1>
+        <form className='row g-3'>   
+             <div className="col-md-6">
+                <label className='form-label'>Título</label>
+                <input
+                    name='titulo'
+                    value={atividade.titulo}
                     onChange={inputTextHandler} 
+                    id='titulo' 
                     type="text" 
                     className="form-control" 
-                    value={atividade.id} 
                 />
-            </div>
+            </div>        
             <div className="col-md-6">
                 <label className='form-label'>Prioridade</label>
                 <select 
@@ -41,21 +67,10 @@ export default function AtividadeForm(props) {
                     <option value='2'>Normal</option>
                     <option value='3'>Alta</option>
                 </select>
-            </div>
-            <div className="col-md-6">
-                <label className='form-label'>Título</label>
-                <input
-                    name='titulo'
-                    value={atividade.titulo}
-                    onChange={inputTextHandler} 
-                    id='titulo' 
-                    type="text" 
-                    className="form-control" 
-                />
-            </div>
-            <div className="col-md-6">
+            </div>           
+            <div className="col-md-12">
                 <label className='form-label'>Descrição</label>
-                <input 
+                <textarea 
                     name='descricao'
                     value={atividade.descricao}
                     onChange={inputTextHandler} 
@@ -66,8 +81,35 @@ export default function AtividadeForm(props) {
             </div>
             <hr />
             <div className='col-12'>
-                <button className='btn btn-outline-secondary' onClick={props.addAtividade}>+ Atividade</button>
+                {
+                    atividade.id === 0 ? (
+                    <button 
+                        className='btn btn-outline-secondary' 
+                        onClick={props.addAtividade}
+                    >
+                            <i className='fas fa-plus me-2'></i>
+                            Atividade
+                    </button>
+                    ) : (
+                    <>
+                       <button 
+                            className='btn btn-outline-success me-2' 
+                            type='submit'
+                        >
+                            <i className='fas fa-plus me-2'></i>
+                            Salvar
+                        </button>
+                        <button 
+                            className='btn btn-outline-warning' 
+                            onClick={handleCancelar}
+                            >
+                                <i className='fas fa-plus me-2'></i>
+                            Cancelar
+                        </button> 
+                    </>
+                )}                
             </div>
         </form>
+        </>
     )
 }
