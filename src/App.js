6f3b150dc90cae11877d6564ea8654d1aaa06bf9
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-22 23:22:23 -0300

    dividing the project in components and making the proper adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './App.css';
+import AtividadeForm from './components/AtividadeForm';
+import Atividade from './components/Atividade';
 
 let initialState = [
   {
@@ -32,91 +34,24 @@ function App() {
     setAtividades([ ...atividades, { ...atividade }]);   
   }
 
-  function prioridadeLabel(param){
-    switch(param) {
-      case '1':
-        return 'Baixa';
-      case '2':
-        return 'Normal';
-      case '3':
-        return 'Alta';
-      default:
-        return 'Não definido';
-    }
-  }
-
-  function prioridadeStyle(param, icon){
-    switch(param) {
-      case '1':
-        return icon ? 'smile' : 'sucess';
-      case '2':
-        return icon ? 'meh' : 'dark';
-      case '3':
-        return icon ? 'frown' : 'warning';
-      default:
-        return 'Não definido';
-    }
-  }
+  function deletarAtividade(id) {
+    const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+    setAtividades([...atividadesFiltradas]);
+  }  
 
   return (
     <>
-    <form className='row g-3'>
-      <div className="col-md-6">
-        <label className='form-label'>Id</label>
-        <input id='id' type="text" className="form-control" readOnly value={Math.max.apply(Math, atividades.map(item => item.id)) + 1} />
-      </div>   
-      <div className="col-md-6">
-        <label className='form-label'>Prioridade</label>
-          <select id="prioridade" className="form-select">
-            <option defaultValue='0'>Selecionar...</option>
-            <option value='1'>Baixa</option>
-            <option value='2'>Normal</option>
-            <option value='3'>Alta</option>
-          </select>
-      </div> 
-      <div className="col-md-6">
-        <label className='form-label'>Título</label>
-        <input id='titulo' type="text" className="form-control" />
-      </div> 
-      <div className="col-md-6">
-        <label className='form-label'>Descrição</label>
-        <input id='descricao' type="text" className="form-control" />
-      </div>  
-      <hr/>  
-      <div className='col-12'>
-        <button className='btn btn-outline-secondary' onClick={addAtividade}>+ Atividade</button>      
-      </div>     
-    </form>
+    <AtividadeForm 
+      addAtividade={addAtividade}
+      atividades={atividades}
+    />
     <div className="mt-3">      
         {atividades.map((ativ) => (         
-          <div key={ativ.id} className={"card mb-2 shadow-sm border-" + prioridadeStyle(ativ.prioridade)}>            
-            <div className="card-body">
-              <div className='d-flex justify-content-between'>
-                <h5 className='card-title'>
-                  <span className='badge bg-secondary me-1'>{ativ.id}</span>
-                   - {ativ.titulo}
-                </h5>
-                <h6>
-                  Prioridade:
-                  <span className={'ms-1 text-' + prioridadeStyle(ativ.prioridade)}>
-                    <i className={'me-1 far fa-' + prioridadeStyle(ativ.prioridade, true)}></i>
-                    {prioridadeLabel(ativ.prioridade)}
-                  </span>                  
-                </h6>
-              </div>
-              <p className="card-text">{ativ.descricao}</p>
-              <div className='d-flex justify-content-end pt-2 m-0 border-top'>
-                <button className='btn btn-sm btn-outline-primary me-2'>
-                  <i className='fas fa-pen me-2'></i>
-                  Editar
-                </button>
-                <button className='btn btn-sm btn-outline-danger'>
-                <i className='fas fa-trash me-2'></i>
-                  Deletar
-                </button>
-              </div>          
-            </div>
-          </div>
+          <Atividade 
+            key={ativ.id} 
+            ativ={ativ}
+            deletarAtividade={deletarAtividade}
+          />
         ))}                 
       
     </div>
